7f0fba5201efb3e2a2954deb1c13827b606dd18e
darogan <darogan@gmail.com>
2019-07-31 12:50:53 +0100

    Updates to accessibility

---
+++ StructureAccessibility/rnaplfold.runner.sh
@@ -0,0 +1,13 @@
+
+
+RNAplfold -T 37 -W 75 -u 10  < DQA101.fasta
+RNAplfold -T 37 -W 75 -u 10  < DQA105.fasta
+
+RNAplfold -T 37 -W 75 -u 10  < DQB105.fasta 
+RNAplfold -T 37 -W 75 -u 10  < DQB102.fasta
+
+cp DQA101_lunp DQA101_lunp.txt
+cp DQA105_lunp DQA105_lunp.txt
+
+cp DQB105_lunp DQB105_lunp.txt
+cp DQB102_lunp DQB102_lunp.txt
